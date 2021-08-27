baee758ed36d80f7458987d7e6b4638726102bda
avinoamn <avi058532@gmail.com>
2021-08-28 02:19:52 +0300

    wip

--- packages/create-express-app/createExpressApp.js
+++ packages/create-express-app/createExpressApp.js
@@ -1,6 +1,8 @@
+const {argsParser} = require('./utils/argsUtils');
 const {initProjectDirectory, initPackageJson, installDependencies, copyTemplates} = require('./utils/shellUtils');
 
 const projectName = process.argv[2];
+const args = argsParser(process.argv.slice(3));
 
 function init() {
     initProjectDirectory(projectName);
--- packages/create-express-app/utils/argsUtils.js
+++ packages/create-express-app/utils/argsUtils.js
@@ -2,10 +2,40 @@ const dependencies = {
     default: ['express', 'helmet', 'cors', 'compression', 'body-parser', 'winston']
 };
 
+const argsMap = {
+    session: {
+        dependencies: [],
+        db: ''
+    },
+    passport: {
+        dependencies: [],
+        db: '',
+    },
+    mongo: {
+        dependencies: [],
+        models: {}
+    }
+};
+
+function argsParser(args) {
+    args.reduce((acc, curr, index) => {
+        // Check if current is a valid argument
+        if (curr.startsWith('--') && !argsMap[curr]) {
+            throw new Error(`Invalid arg: ${curr}`);
+        }
+
+        return curr.startsWith('--') ? {
+            ...acc,
+            [curr.substr(2)]: argsMap[curr.substr(2)].getArgValue(acc[index + 1])
+        } : acc;
+    }, {});
+}
+
 function getDependencies() {
     return dependencies.default;
 }
 
 module.exports = {
-    getDependencies
+    getDependencies,
+    argsParser
 };
