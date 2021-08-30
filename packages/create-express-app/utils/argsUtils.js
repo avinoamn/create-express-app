daefc5cf5eb48f2c71e3837353caf7947ddf3d49
Avinoam Naamat <avi058532@gmail.com>
2021-08-30 13:52:52 +0300

    Added argsParser (#14)
    * wip
    
    * wip
    
    * wip
    
    * moved consts to const folder
    
    * wip
    
    * Finished adding argsParser

--- packages/create-express-app/utils/argsUtils.js
+++ packages/create-express-app/utils/argsUtils.js
@@ -1,11 +1,45 @@
-const dependencies = {
-    default: ['express', 'helmet', 'cors', 'compression', 'body-parser', 'winston']
-};
+const {valuesMap, dependenciesMap} = require('../consts');
 
-function getDependencies() {
-    return dependencies.default;
+function getArgDependencies(arg, nextArg) {
+    return [...dependenciesMap[arg].default, ...(dependenciesMap[arg][nextArg] || [])];
+}
+
+function getArgValue(arg, nextArg) {
+    const isValue = nextArg && !nextArg.startsWith('--');
+    
+    return isValue ?
+        valuesMap[arg].valueParser(nextArg) :
+        valuesMap[arg].defaultValue;
+}
+
+function argsParser(args) {
+    return args.reduce((acc, curr, index) => {
+        const isArg = curr.startsWith('--');
+        const arg = curr.substr(2);
+        const nextArg = acc[index + 1];
+
+        // Check if current is a valid argument
+        if (isArg && !valuesMap[arg]) {
+            throw new Error(`Invalid arg: ${arg}`);
+        }
+
+        return isArg ? {
+            ...acc,
+            [arg]: {
+                dependencies: getArgDependencies(arg, nextArg),
+                value: getArgValue(arg, nextArg)
+            }
+        } : acc;
+    }, {});
+}
+
+function getDependencies(args) {
+    return Object.keys(args).reduce((deps, currArg) => (
+        [...deps, ...args[currArg].dependencies]
+    ), dependenciesMap.default);
 }
 
 module.exports = {
-    getDependencies
+    getDependencies,
+    argsParser
 };
