b6b01e8836d21f8be2788695e4a5958eede6e610
avinoamn <avi058532@gmail.com>
2021-10-13 14:04:14 +0300

    Fixed imports and function use according to changes

--- packages/create-express-app/createExpressApp.js
+++ packages/create-express-app/createExpressApp.js
@@ -1,14 +1,15 @@
-const {argsParser, handleArgs} = require('./utils/argsUtils');
+const {parseArgs} = require('./utils/parseUtils');
+const {handleArgs} = require('./utils/argsUtils');
 const {initProjectDirectory, initPackageJson, installDependencies, copyTemplates} = require('./utils/shellUtils');
 
 const projectName = process.argv[2];
-const args = argsParser(process.argv.slice(3));
+const args = parseArgs(process.argv.slice(3));
 
 function init() {
     initProjectDirectory(projectName);
     initPackageJson();
     installDependencies(args);
-    copyTemplates('../resources/templates/must', './', ['.gitignore', 'index.js', 'src']);
+    copyTemplates('must', './', ['.gitignore', 'index.js', 'src']);
     handleArgs(args);
 }
 
